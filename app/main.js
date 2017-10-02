019dbba523801e0def5e8875e825063c02f66638
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-02 15:03:37 -0400

    tooltip working and basic styling

--- app/main.js
+++ app/main.js
@@ -7,13 +7,15 @@ var demo = new Vue({
 
 	// Define some data and properties
 	data: {
-		show_tooltip: false,
-		text_content: 'Hello, world!'
-	}
+		show_tooltip: true,
+		text_content: 'Hello world!'
+	},
 
-	// Define some functions
+	// Define some functions (functions go in methods)
 	methods: {
-		
+		toggleTooltip: function() {
+			this.show_tooltip = !this.show_tooltip;
+		}
 	}
 
 
