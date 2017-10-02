7bb7908fca7316f2435c8a97175c9040cc3e6d1e
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-02 14:33:22 -0400

    main.js has basics set up that may be reusable for other projects.  committing as a bookmark.

---
+++ app/main.js
@@ -0,0 +1,20 @@
+// Create a Vue instance
+
+var demo = new Vue({
+
+	// A DOM element to mount our initial view model
+	el: '#main',
+
+	// Define some data and properties
+	data: {
+		show_tooltip: false,
+		text_content: 'Hello, world!'
+	}
+
+	// Define some functions
+	methods: {
+		
+	}
+
+
+});
